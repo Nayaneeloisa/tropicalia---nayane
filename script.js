7d48aa7fd715cc11556a2ba5310cdcf476a18e72
Nayaneeloisa <160730708+Nayaneeloisa@users.noreply.github.com>
2025-06-04 18:07:36 +0000

    corrigido

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const aumentaFonteBotao = document.getElementById ('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
     const alternaContraste = document.getElementById('alterna-contraste');
     const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
     botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
-
+    
+    let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     });
     
-    let tamanhoAtualFonte = 1;
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     });
 
     alternaContraste.addEventListener('click', function(){
         document.body.classList.toggle('alto-contraste')
-})
+    });
 
-
-        botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.addEventListener('click', function (){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-       })
-
+        
+   });
 });
-
 ScrollReveal().reveal('#inicio', { delay: 500 });
 ScrollReveal().reveal('#tropicalia', { delay: 500 });
 ScrollReveal().reveal('#galeria', { delay: 500 });
